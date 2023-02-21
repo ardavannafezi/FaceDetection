576eac4273797ebdc4ef1876129d1ada94ddfcfe
a.nafezi <ardavannafezi@gmail.com>
2023-02-22 00:35:21 +0330

    multiface added

--- FaceDetector.py
+++ FaceDetector.py
@@ -3,8 +3,16 @@ import cv2
 trData  = cv2.CascadeClassifier('TRData.xml')
 
 img = cv2.imread("FaceImages/2.jpg")
+grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faceCordinate = trData.detectMultiScale(grayscale)
+
+
+for(x,y,w,h) in faceCordinate:
+    cv2.rectangle(img, (x, y), (x+w , y+h), (0,255,0), 5)
+
 
 cv2.imshow('ck', img)
-cv2.waitKey()
 
+cv2.waitKey()
 print("run")
